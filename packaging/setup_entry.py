b8475fef7d3a945e0e790c1c514a14860e305ebe
Rapt0r06300 <bermond.florent06@gmail.com>
2026-09-06 16:31:04 +0200

    feat(setup): route double click into graphical setup

--- packaging/setup_entry.py
+++ packaging/setup_entry.py
@@ -1,2 +1,4 @@
-from alinacoder.product.installer import main
-if __name__=='__main__':raise SystemExit(main())
+from alinacoder.product.setup_gui import setup_entrypoint
+
+if __name__ == "__main__":
+    raise SystemExit(setup_entrypoint())
