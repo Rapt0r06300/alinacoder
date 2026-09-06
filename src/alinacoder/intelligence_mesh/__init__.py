5f00135f87ae3606a0eb9bab5b748386d4cafbd7
Rapt0r06300 <bermond.florent06@gmail.com>
2026-09-06 14:51:30 +0200

    feat(v0.2): export provider atlas contracts

--- src/alinacoder/intelligence_mesh/__init__.py
+++ src/alinacoder/intelligence_mesh/__init__.py
@@ -1,6 +1,7 @@
 from .catalog import CatalogDrift, ProviderCatalog
 from .continuity import ContinuityEnvelope
 from .models import CapabilityRequirement, CostProofReceipt, ModelRoute, RouteUnavailableError, StaleResponseError
+from .provider_atlas import ProviderAtlas, ProviderDefinition, ProviderSafetyClass, normative_provider_atlas
 from .routing import FrontierRouter
 
 __all__ = [
@@ -10,7 +11,11 @@ __all__ = [
     "CostProofReceipt",
     "FrontierRouter",
     "ModelRoute",
+    "ProviderAtlas",
     "ProviderCatalog",
+    "ProviderDefinition",
+    "ProviderSafetyClass",
     "RouteUnavailableError",
     "StaleResponseError",
+    "normative_provider_atlas",
 ]
