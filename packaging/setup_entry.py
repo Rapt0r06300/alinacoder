bb5d4f0a265c337e59277450ae056b8935559413
Rapt0r06300 <bermond.florent06@gmail.com>
2026-09-06 19:29:02 +0200

    fix(ci): surface packaged bootstrap blocker code

--- packaging/setup_entry.py
+++ packaging/setup_entry.py
@@ -1,4 +1,8 @@
+import sys
+
+from alinacoder.product.ci_exit_codes import translate_setup_exit_code
 from alinacoder.product.setup_gui import setup_entrypoint
 
 if __name__ == "__main__":
-    raise SystemExit(setup_entrypoint())
+    args = list(sys.argv[1:])
+    raise SystemExit(translate_setup_exit_code(setup_entrypoint(args), args))
