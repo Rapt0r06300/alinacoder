37154428c674e2d1c64bc281bf4c490fb214889a
Rapt0r06300 <bermond.florent06@gmail.com>
2026-09-06 14:56:19 +0200

    feat(v0.2): export provider runtime contracts

--- src/alinacoder/intelligence_mesh/__init__.py
+++ src/alinacoder/intelligence_mesh/__init__.py
@@ -2,6 +2,7 @@ from .catalog import CatalogDrift, ProviderCatalog
 from .continuity import ContinuityEnvelope
 from .models import CapabilityRequirement, CostProofReceipt, ModelRoute, RouteUnavailableError, StaleResponseError
 from .provider_atlas import ProviderAtlas, ProviderDefinition, ProviderSafetyClass, normative_provider_atlas
+from .providers import GeminiProvider, HttpResult, OllamaProvider, OpenAICompatibleProvider, ProviderError, ProviderModel, ProviderResponse, ZeroCostProvider
 from .routing import FrontierRouter
 
 __all__ = [
@@ -10,12 +11,20 @@ __all__ = [
     "ContinuityEnvelope",
     "CostProofReceipt",
     "FrontierRouter",
+    "GeminiProvider",
+    "HttpResult",
     "ModelRoute",
+    "OllamaProvider",
+    "OpenAICompatibleProvider",
     "ProviderAtlas",
     "ProviderCatalog",
     "ProviderDefinition",
+    "ProviderError",
+    "ProviderModel",
+    "ProviderResponse",
     "ProviderSafetyClass",
     "RouteUnavailableError",
     "StaleResponseError",
+    "ZeroCostProvider",
     "normative_provider_atlas",
 ]
