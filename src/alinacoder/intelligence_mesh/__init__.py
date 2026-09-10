7d869a343d1625c5cae3a1435cabbc41cc0875e0
Rapt0r06300 <bermond.florent06@gmail.com>
2026-09-10 23:50:39 +0200

    feat(mesh): export routing intelligence primitives

--- src/alinacoder/intelligence_mesh/__init__.py
+++ src/alinacoder/intelligence_mesh/__init__.py
@@ -3,12 +3,17 @@ from .continuity import ContinuityEnvelope
 from .experiential import ExperientialProvider
 from .fabric import InferenceFabric
 from .models import CapabilityRequirement, CostProofReceipt, ModelRoute, RouteUnavailableError, StaleResponseError
+from .profiles import CapabilityObservation, CapabilityProfileStore, ModelCapabilityProfile, wilson_lower_bound
 from .provider_atlas import ProviderAtlas, ProviderDefinition, ProviderSafetyClass, normative_provider_atlas
 from .providers import GeminiProvider, HttpResult, OllamaProvider, OpenAICompatibleProvider, ProviderError, ProviderModel, ProviderResponse, ZeroCostProvider
 from .qualification import QualificationRegistry, SponsoredCreditQualification, ZeroCostQualification
+from .quota import QuotaLedger, QuotaRecord, QuotaState
 from .routing import FrontierRouter
+from .tasks import TaskClass, TaskClassifier
 
 __all__ = [
+    "CapabilityObservation",
+    "CapabilityProfileStore",
     "CapabilityRequirement",
     "CatalogDrift",
     "ContinuityEnvelope",
@@ -18,6 +23,7 @@ __all__ = [
     "GeminiProvider",
     "HttpResult",
     "InferenceFabric",
+    "ModelCapabilityProfile",
     "ModelRoute",
     "OllamaProvider",
     "OpenAICompatibleProvider",
@@ -29,10 +35,16 @@ __all__ = [
     "ProviderResponse",
     "ProviderSafetyClass",
     "QualificationRegistry",
+    "QuotaLedger",
+    "QuotaRecord",
+    "QuotaState",
     "RouteUnavailableError",
     "SponsoredCreditQualification",
     "StaleResponseError",
+    "TaskClass",
+    "TaskClassifier",
     "ZeroCostProvider",
     "ZeroCostQualification",
     "normative_provider_atlas",
+    "wilson_lower_bound",
 ]
